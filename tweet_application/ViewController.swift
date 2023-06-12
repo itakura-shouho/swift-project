94fb6d551ea334a740cfeab1f5498063e7e12573
itakura shouho <user@usernoMacBook-Pro.local>
2023-06-12 15:07:49 +0900

    1st

--- tweet_application/ViewController.swift
+++ tweet_application/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var postText: UITextField!
+    
+    @IBOutlet weak var viewTitle: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onPost(_ sender: Any) {
+    }
+    
 }
 
