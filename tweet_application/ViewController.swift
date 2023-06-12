49c51bedb167a9e9835d445977ef5e9d495d3bc1
itakura shouho <user@usernoMacBook-Pro.local>
2023-06-12 14:10:25 +0900

    Initial Commit

---
+++ tweet_application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tweet_application
+//
+//  Created by itakura shouho on 2023/06/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
